0814a8123d6936396ff74f7a8f950ac14270cec1
div0ky <wildfireajs@gmail.com>
2020-03-20 14:28:38 -0500

    200320-142837

--- IdleBot/bot_functions/AdvancedFunctions.py
+++ IdleBot/bot_functions/AdvancedFunctions.py
@@ -1,7 +1,7 @@
 import pyautogui
 from bot_internals.GameCoords import game_coords
 from bot_internals.DatabaseManager import database
-from bot_internals.SharedFunctions import ocr, is_num
+from bot_internals.SharedFunctions import ocr, is_num, change_upgrade_progression
 from bot_internals.BotLog import log
 import re, ast
 import time
@@ -63,3 +63,109 @@ def map_missions():
             log.info("Going home.")
 
         pyautogui.press('esc')
+
+
+def auto_prestige():
+    if database.auto_prestige and time.time() >= database.prestige_check_time:
+        time.sleep(0.5)
+        pyautogui.click(game_coords.town_coords)
+        time.sleep(0.5)
+        pyautogui.click(game_coords.temple_of_eternals_coords)
+        time.sleep(0.5)
+        pyautogui.click(game_coords.relative_coords(1359, 560))  # Open prestige menu
+        time.sleep(0.5)
+
+        pyautogui.screenshot(database.ocr_image, region=(game_coords.relative_coords(1070, 745, 160, 70)))
+        result = ocr(database.ocr_image)
+
+        # while not isNum(result, 2):
+        #     pyautogui.screenshot(database.OCR_IMAGE, region=(relCoords(1070, 745, 160, 70)))
+        #     result = ocr(database.OCR_IMAGE)
+
+        if is_num(result, 2):
+            database.save_option('ocr_succeed_count', int(database.read_option('ocr_succeed_count')) + 1)
+
+            prestige_level = round(float(result[1:]), 2)
+
+        else:
+            database.save_option('ocr_fail_count', int(database.read_option('ocr_fail_count')) + 1)
+            log.warning("Wasn't able to ascertain our current prestige level.")
+            # TODO: Determine if this is still necessary. DISABLED for now.
+            # if db.ocr_f_pct > 50:
+            #     pyautogui.screenshot(os.path.expanduser("~") + f"/Documents/Firestone Bot/OCR/Fail_{db.ocr_fail_count}_{round(time(), 5)}.png")
+
+        if prestige_level:
+            progress = round((prestige_level / database.prestige_level) * 100)
+            log.info(f"Current earnings are at {prestige_level}x which is {progress}% of our goal.")
+            time.sleep(0.5)
+
+            snooze = ((progress - 100) / (0 - 100)) * ((60 - 1) + 1) * 1000
+            if snooze <= 1000:
+                snooze = 1000
+            elif snooze >= 60000:
+                snooze = 60000
+            database.save_option('prestige_check_time', time.time() + snooze)
+            log.info(f"Will wait {round((snooze / 1000), 2)}min before checking Prestige progress again.")
+            time.sleep(0.5)
+
+            if prestige_level >= database.prestige_level:
+                log.info("Firestone earnings are satisfactory. Prestiging...")
+                pyautogui.click(game_coords.relative_coords(1160, 525))  # Click on FREE prestige option
+                time.sleep(15)  # Wait for prestige to finish
+
+                pyautogui.click(game_coords.guardian_click_coords, clicks=20, interval=1.1)  # Let's get some gold to buy party with
+
+                setup_party()
+                change_upgrade_progression('Milestone')
+
+        log.info("Going back to home screen.")
+        pyautogui.click(game_coords.big_close_coords, clicks=3, interval=0.5)  # Go home because we're not prestiging
+
+
+def setup_party():
+    log.info("Setting up the party.")
+    time.sleep(0.5)
+    pyautogui.click(game_coords.party_coords)
+
+    if database.party_size >= 1:
+        time.sleep(0.5)
+        log.info("Buying first party slot.")
+        pyautogui.click(game_coords.relative_coords(802, 795))  # Buy fist party slot
+    if database.party_size >= 2:
+        time.sleep(0.5)
+        log.info("Buying second party slot.")
+        pyautogui.click(game_coords.relative_coords(775, 540))  # Buy second slot
+    if database.party_size >= 3:
+        time.sleep(0.5)
+        log.info("Buying third party slot.")
+        pyautogui.click(game_coords.relative_coords(575, 815))  # Buying third party slot
+    if database.party_size >= 4:
+        time.sleep(0.5)
+        log.info("Buying fourth party slot.")
+        pyautogui.click(game_coords.relative_coords(550, 445))  # Buying fourth party slot
+    if database.party_size >= 5:
+        time.sleep(0.5)
+        log.info("Buying fifth party slot.")
+        pyautogui.click(game_coords.relative_coords(510, 630))  # Buying fifth party slot
+
+    if database.party_size >= 1:
+        time.sleep(0.5)
+        pyautogui.click(game_coords.hero_coords[database.party_slot_1])
+    if database.party_size >= 2:
+        time.sleep(0.5)
+        pyautogui.click(game_coords.hero_coords[database.party_slot_2])
+    if database.party_size >= 3:
+        time.sleep(0.5)
+        pyautogui.click(game_coords.hero_coords[database.party_slot_3])
+    if database.party_size >= 4:
+        time.sleep(0.5)
+        pyautogui.click(game_coords.hero_coords[database.party_slot_4])
+    if database.party_size >= 5:
+        time.sleep(0.5)
+        pyautogui.click(game_coords.hero_coords[database.party_slot_5])
+
+    time.sleep(0.5)
+    pyautogui.click(game_coords.relative_coords(1160, 95))  # Click to save changes
+    time.sleep(0.5)
+    pyautogui.click(game_coords.big_close_coords)
+    time.sleep(0.5)
