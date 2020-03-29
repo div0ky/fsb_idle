3a2eedf31c42529d66865da2123759c320644511
div0ky <wildfireajs@gmail.com>
2020-03-29 15:34:16 -0500

    200329-153414

--- IdleBot/bot_internals/version_info.py
+++ IdleBot/bot_internals/version_info.py
@@ -3,7 +3,7 @@
 vMajor = 0  # Increments on a BREAKING change
 vMinor = 8  # Increments on a FEATURE change
 vPatch = 0  # Increments on a FIX / PATCH
-vStage = "dev.5"
+vStage = "stable"
 full_version = f"{vMajor}.{vMinor}.{vPatch}-{vStage}"  # Should be self explanatory
 version = f"{vMajor}.{vMinor}.{vPatch}"  # Should be self explanatory
 
